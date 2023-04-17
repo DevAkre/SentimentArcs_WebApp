ae8dc1efc0273c82614ecea07113597d74823b12
DevAkre <akre1@kenyon.edu>
2023-04-17 17:30:33 -0400

    text select api

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import random
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -19,4 +20,50 @@ class User(db.Model):
         self.token = random.randint(0, 2**31 - 1)
         
     def generate_access_token(self):
-        token = random.randint(0, 2**31 - 1)
+        self.token = random.randint(0, 2**31 - 1)
+
+class Model(db.Model):
+    model_id = db.Column(db.Integer, primary_key=True)
+    model_name = db.Column(db.String(80), unique=True, nullable=False)
+    family = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    text_id = db.Column(db.Integer, db.ForeignKey('text.text_id'), nullable=False)
+
+    def __repr__(self):
+        return '<Model %r>' % self.name
+    
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+class Text(db.Model):
+    text_id = db.Column(db.Integer, primary_key=True)
+    date_uploaded = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    size = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    author = db.Column(db.String(80), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    filename = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Text %r>' % self.text
+    
+    def __init__(self, title, author, user_id, size, filename ):
+        self.title = title
+        self.author = author
+        self.user_id = user_id
+        self.filename = filename
+        self.size = size
+
+class Cleaned_Text(db.Model):
+    cleaned_text_id = db.Column(db.Integer, primary_key=True)
+    text_id = db.Column(db.Integer, db.ForeignKey('text.text_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    sentences = db.Column(db.JSON, nullable=False)
+
+    def __repr__(self):
+        return '<Cleaned_Text %r>' % self.text
+    
+    def __init__(self, text, model_id):
+        self.text = text
+        self.model_id = model_id
--- server.py
+++ server.py
@@ -1,15 +1,30 @@
 from flask import Flask, request
-from models import db, User
+from models import db, User, Text
+import os
+import json
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf'}
 # create the app
 app = Flask(__name__, static_url_path='')
 #init config
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+#make upload folder if it doesn't exist
+os.makedirs(os.path.join(app.config['UPLOAD_FOLDER']), exist_ok=True)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
     db.session.commit()
+
+def get_user_from_token(given_token):
+    return User.query.filter_by(token=given_token).first()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def serve_frontEnd():
@@ -41,7 +56,7 @@ def user_login():
 
 @app.post('/api/user/logout')
 def user_logout():
-    return
+    return {}
 
 @app.post('/api/user/register')
 def user_register():
@@ -61,8 +76,68 @@ def user_register():
     else:
         return {
             "success": False,
-            "error": "Username already exists"
+            "error": "Username already taken"
         }
+
+@app.post('/api/text/upload')
+def text_upload():
+    user = get_user_from_token(request.form.get("token"))
+    if(user is None):
+        return {"success": False, "error": "Invalid token"}
+    else:
+        print(user.username + " upload attempted!")
+        if 'file' not in request.files:
+            return {"success": False, "error": "No valid file selected"}
+        file = request.files.get('file')
+        if file.filename == '':
+            return {"success": False, "error": "No selected file"}
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            while(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename))):
+                filename = "copy_of_" + filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_size = os.path.getsize(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            text = Text(request.form.get("title"), request.form.get("author"), user.user_id, file_size, filename)
+            db.session.add(text)
+            db.session.commit()
+            return {"success": True}
+        
+@app.post('/api/text/delete')
+def text_delete():
+    user = get_user_from_token(request.form.get("token"))
+    if(user is None):
+        return {"success": False, "error": "Invalid token"}
+    else:
+        print(user.username + " delete attempted!")
+        text = Text.query.filter_by(text_id=request.form.get("text_id")).first()
+        if(text is None):
+            return {"success": False, "error": "Invalid text id"}
+        else:
+            if(text.user_id != user.user_id):
+                return {"success": False, "error": "You do not own this text"}
+            else:
+                db.session.delete(text)
+                db.session.commit()
+                return {"success": True}
+
+@app.post('/api/text/list')
+def text_list():
+    request_data = request.get_json()
+    user = get_user_from_token(request_data["token"])
+    if(user is None):
+        return {"success": False, "error": "Invalid token"}
+    else:
+        print("fetching list for user "+ user.username)
+        print(user.username + " list attempted!")
+        texts = Text.query.filter_by(user_id=user.user_id).order_by(Text.date_uploaded.desc()).all()
+        if(texts is None):
+           return {"success": False, "error": "No texts found"}
+        textList = []
+        for text in texts:
+            textList.append({"text_id":text.text_id, "date_uploaded":text.date_uploaded, "size":text.size, "title": text.title, "author" : text.author, "filename":text.filename})
+        return {"success": True, "texts": json.dumps(textList, default = str)}
+    
+
 
 if __name__ == '__main__':
     app.run()
